9ef1bb1a87fcd1f5bfd8d015905a5f16e159f438
heptaluan <vd2o@foxmail.com>
2023-02-24 14:59:42 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { renderRoutes } from 'react-router-config'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import routes from './config'
 
 const Router = () => {
   return (
     <>
-      <HashRouter basename="/">{renderRoutes(routes)}</HashRouter>
+      <BrowserRouter basename='/ct/'>{renderRoutes(routes)}</BrowserRouter>
     </>
   )
 }
